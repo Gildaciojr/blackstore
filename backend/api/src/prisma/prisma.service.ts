92919bc1c7a20169a92b394b727a92761d4e9e2a
Gildacio <gildacioribeiro@outlook.com.br>
2026-03-30 17:10:17 -0300

    feat: payment pix integration + webhook + checkout flow completo0132

--- backend/api/src/prisma/prisma.service.ts
+++ backend/api/src/prisma/prisma.service.ts
@@ -1,21 +1,13 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    const pool: Pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-    });
-
-    const adapter = new PrismaPg(pool);
-
-    super({ adapter });
-  }
-
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit(): Promise<void> {
     await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
   }
 }
