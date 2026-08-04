194dfd22d6f0efc76062aaa40131d644f117b19b
Gildacio <gildacioribeiro@outlook.com.br>
2026-08-04 16:05:10 -0300

    fix: adiciona decorators no update-customer dto para liberar checkout

--- backend/api/src/customer/dto/update-customer.dto.ts
+++ backend/api/src/customer/dto/update-customer.dto.ts
@@ -1,6 +1,19 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class UpdateCustomerDto {
+  @IsString()
+  @IsOptional()
   name?: string;
+
+  @IsString()
+  @IsOptional()
   surname?: string;
+
+  @IsString()
+  @IsOptional()
   phone?: string;
+
+  @IsString()
+  @IsOptional()
   cpf?: string;
 }
