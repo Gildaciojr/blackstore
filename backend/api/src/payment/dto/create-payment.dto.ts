8262abd1d0bbbd43120026dbc5d3bb70030b9080
Gildacio <gildacioribeiro@outlook.com.br>
2026-08-04 16:30:12 -0300

    fix: adiciona decorators no update-customer dto para liberar checkout010

--- backend/api/src/payment/dto/create-payment.dto.ts
+++ backend/api/src/payment/dto/create-payment.dto.ts
@@ -1,9 +1,23 @@
+import { IsIn, IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class CreatePaymentDto {
+  @IsString()
   orderId!: string;
+
+  @IsString()
+  @IsIn(['pix', 'card'])
   method!: 'pix' | 'card';
 
   // CARTÃO
+  @IsString()
+  @IsOptional()
   cardToken?: string;
+
+  @IsNumber()
+  @IsOptional()
   installments?: number;
+
+  @IsString()
+  @IsOptional()
   holderName?: string;
 }
